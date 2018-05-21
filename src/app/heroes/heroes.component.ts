13d3f66fa3b7d7445717fcb3f1f8872ac306f095
zaxbatch <34843086+zaxbatch@users.noreply.github.com>
2018-05-21 17:38:04 -0400

    end of section 6
    Added messages component and message service. As a bonus the titles are lined up now

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,9 +10,10 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
+  selectedHero: Hero;
+
   heroes: Hero[];
 
-  selectedHero: Hero;
 
 
   constructor(private heroService: HeroService) { }
@@ -26,6 +27,7 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-  this.heroes = this.heroService.getHeroes();
-}
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 }
