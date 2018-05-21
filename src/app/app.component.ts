c6d1164a03aaa16f7508bfbbe4929cb0c8533188
zaxbatch <34843086+zaxbatch@users.noreply.github.com>
2018-05-21 15:15:47 -0400

    end of section 4

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Angular Tour of Heroes';
+  heading01 = 'Built by Zerric Dotcom';
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Jacko' },
+  { id: 12, name: 'Bark Man' },
+  { id: 13, name: 'TomTom' },
+  { id: 14, name: 'Mega Girl' },
+  { id: 15, name: 'Slippery Sue' },
+  { id: 16, name: 'Stage Setter' },
+  { id: 17, name: 'Ms. Blast' },
+  { id: 18, name: 'Blingo' },
+  { id: 19, name: 'Jo Mom' },
+  { id: 20, name: 'Spinner' }
+];
