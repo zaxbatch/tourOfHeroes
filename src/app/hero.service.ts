b005503b175c559c355c52151e3af4c272887354
zaxbatch <34843086+zaxbatch@users.noreply.github.com>
2018-05-22 17:47:48 -0400

    add heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -46,6 +46,14 @@ updateHero (hero: Hero): Observable<any> {
   );
 }
 
+/** POST: add a new hero to the server */
+addHero (hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, httpOptions).pipe(
+    tap((hero: Hero) => this.log(`added hero w/ id=${hero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
+
 /*
   getHero(id: number): Observable<Hero> {
   // TODO: send the message _after_ fetching the hero
